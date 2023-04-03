ef046001dd45192abef8110a82d7704ffd81c8cf
Mark-Andre17 <84619111+Mark-Andre17@users.noreply.github.com>
2023-04-03 22:37:02 +0300

    commit new tasks

--- HW_functions.py
+++ HW_functions.py
@@ -8,11 +8,9 @@ def get_some_number(a, b):
         return max(new_list)
 
 
-print(get_some_number(102, 104))
-
-
 # задание 2 Напишите функцию, которая получает на входе строку из двух слов, и возвращает True если оба слова
 # начинаются с одной и той же буквы.
+
 def get_boolean(string):
     new_list = string.split()
     return new_list[0][0].lower() == new_list[1][0].lower()
@@ -25,8 +23,6 @@ def make_twenty(a, b):
     return a == 20 or b == 20 or a + b == 20
 
 
-print(make_twenty(20, 20))
-
 # Задание 4  Напишите функцию, которая переводит в верхний регистр первую и четвёртую буквы имени.
 
 
@@ -37,8 +33,49 @@ def old_macdonald(name):
             new_list.append(n.upper())
         else:
             new_list.append(n)
-
     return ''.join(new_list)
 
 
-print(old_macdonald('macdonald'))
+# задание 5 На входе фраза, на выходе вернуть слова в обратном порядке
+
+def reverse_string(string):
+    new_list = string.split()
+    return ' '.join(new_list[::-1])
+
+
+# задание 6 на входе число n, вернуть True если n находится не далее чем на 10 от чисел 100 или 200.
+def almost_there(number):
+    return 90 <= number <= 110 or 190 <= number <= 210
+
+
+# задание 7 На входе список чисел, вернуть True если массив содержит где-нибудь 3 рядом с 3.
+
+
+def return_boolean(my_list):
+    for i in range(0, len(my_list) - 1):
+        if my_list[i:i + 2] == [3, 3]:
+            return True
+    return False
+
+
+# задание 8 На входе строка, вернуть строку где каждый символ исходной строки повторяется три раза.
+#
+def paper_doll(string):
+    new_list = []
+    for i in string:
+        new_list.append(i * 3)
+    return ''.join(new_list)
+
+
+# задание 9 На входе три числа от 1 до 11. Если их сумма меньше или равна 21, то вернуть их сумму. Если сумма больше
+# 21 *и* среди чисел есть 11, то уменьшить общую сумму на 10. И наконец, если сумма (в том числе после уменьшения)
+# превышает 21, вернуть 'BUST'.
+
+
+def blackjack(*arg):
+    if sum(arg) > 21 and 11 in arg:
+        return sum(arg) - 10
+    elif sum(arg) <= 21:
+        return sum(arg)
+    else:
+        return 'Buzz'
